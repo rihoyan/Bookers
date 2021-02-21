5be4510e7680ad21d648d0912cc846d11a9679b3
EC2 Default User <ec2-user@ip-172-31-17-115.ec2.internal>
2021-02-21 22:50:23 +0000

    [update]createaction

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def top
+    # top画面からリンクでnewに飛ぶ
   end
 
-  def index
+  def new
+    # 投稿画面に渡すためのインスタンス変数を生成する
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
   end
 
   def show
   end
 
   def edit
+    # 編集画面に渡すためのインスタンス変数を生成する
+  end
+
+  private
+  # 必要な情報だけを保存するために設定する
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
