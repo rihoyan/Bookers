4ad154188b6bce496c073bb198448a27887d6372
EC2 Default User <ec2-user@ip-172-31-17-115.ec2.internal>
2021-02-23 10:18:31 +0000

    [update]uccessfully_message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,50 @@
 class BooksController < ApplicationController
   def top
-    # top画面からリンクでnewに飛ぶ
+    # top画面からリンクでindexに飛ぶ
   end
 
-  def new
+  def index
     # 投稿画面に渡すためのインスタンス変数を生成する
+    # 一覧表示するためのインスタンス変数を生成する
     @book = Book.new
+    @books = Book.all
   end
 
   def create
+    # create bookボタンを押した後に動くアクション
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
+    if book.save
+       flash[:create] = "Book was successfully created！"
+       redirect_to book_path(book)
+    else
+       render "index"
+    end
   end
 
   def show
+    # アクセスされたidのtitleとbodyを取得する
+    @book = Book.find(params[:id])
   end
 
   def edit
     # 編集画面に渡すためのインスタンス変数を生成する
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+       flash[:update] = "Book was successfully update！"
+       redirect_to book_path(book)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+       flash[:destroy] = "Book was successfully destroyed！"
+       redirect_to index_book_path
+    end
   end
 
   private
